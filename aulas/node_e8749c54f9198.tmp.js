c75561a267a9d94e3c1fdb38373b52a0302c7352
Guitis <guilhermetb50@gmail.com>
2023-02-18 11:52:06 -0300

    Learning about repetições

---
+++ aulas/node_e8749c54f9198.tmp.js
@@ -0,0 +1,11 @@
+var c = 1 //c recebe 1
+while (c <= 6) { //(while - enquanto) c for menor ou igual 6
+    console.log ('Tudo') //escreva "Tudo"
+    c++ // c = c + 1 = 2
+}
+
+var e = 1
+while (e = 8) {
+    console.log ('Te amo')
+    e++
+}

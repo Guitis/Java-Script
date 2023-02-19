181694de30fdd48d2e298ac73a4bf0752125f5d6
Guitis <guilhermetb50@gmail.com>
2023-02-19 09:49:19 -0300

    ex16.js

---
+++ aulas/ex16.js
@@ -0,0 +1,11 @@
+/*var c = 1 //Inicio
+while (c <= 10) { //teste
+    console.log('Tudo')
+    c++ //Incremento (incr)
+}
+*/
+//-------------------------------
+console.log('Vai começar')
+for (var c=1/*inicio*/; c<=10 /*teste*/ ; c++ /*incremento*/ ){
+    console.log('Oi')
+}

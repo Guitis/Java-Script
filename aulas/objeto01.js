094693560549f723d7de08eb06620ba445bee5ff
Guilherme Thomazotti Barros <guilhermetb50@gmail.com>
2023-03-25 15:39:51 -0300

    update JS

--- aulas/objeto01.js
+++ aulas/objeto01.js
@@ -8,3 +8,16 @@ engordar (p=0){
 
 amigo.engordar(2)
     console.log(`${amigo.nome} pesa ${amigo.peso} Kg`)
+
+let guitis = {nome: 'Guilherme',
+sexo: 'M',
+peso: 56,
+engordar (p=0) {
+    console.log('Engordou')
+    this.peso += p
+}}
+
+guitis.engordar(5)
+    console.log(`${guitis.nome} pesa ${guitis.peso} Kg`)
+
+    
